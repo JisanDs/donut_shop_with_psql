8691d4e7a7aa411d027d20445d51fbe8e0d0e795
JisanDs <mdjisan01823jis@gmail.com>
2026-07-12 12:38:42 +0600

    init

--- database/schemas/create_schemas.sql
+++ database/schemas/create_schemas.sql
@@ -3,7 +3,7 @@
     Every schema have his owne diractory and every dir containt plan.md file.
 */
 
-create DB change current db to donut_shop.
+-- create DB change current db to donut_shop.
 CREATE DATABASE donut_shop;
 
 create schemas
@@ -11,11 +11,11 @@ CREATE SCHEMA inventory;
 CREATE SCHEMA hr;
 CREATE SCHEMA sales;
 
-set search path for full DB
+-- set search path for full DB
 ALTER DATABASE donut_shop
 SET search_path TO public, inventory, hr, sales;
 
 
-this is global types
+-- this is global types
 CREATE TYPE pay_types AS ENUM('cash', 'm_bk', 'crd', 'bank'); -- crd = card, m_bk = mobile_banking
 CREATE TYPE reasons AS ENUM('sale', 'restock', 'add', 'damaged', 'use');
